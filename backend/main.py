050d952ac9fc3bd868e4bfca7222a0eef8ef9b03
HK <hk5204@126.com>
2026-01-08 12:18:30 +0800

    V0.1.4 backend bug fix

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,7 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
